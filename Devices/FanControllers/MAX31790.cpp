cbbd6530a8d5d47cbb4ee659d2300bd9abd675aa
Jeroen Vandezande <Jeroen.Vandezande@gmail.com>
2022-12-09 10:20:40 +0100

    fixed typo

--- Devices/FanControllers/MAX31790.cpp
+++ Devices/FanControllers/MAX31790.cpp
@@ -68,7 +68,7 @@ namespace LowLevelEmbedded
 
 			bool MAX31790::setFanRateOfChange(uint8_t fanID, MAX31790_RateOfChange rateOfChange)
 			{
-				uint8_t rot = (u_int8_t)rateOfChange;
+				uint8_t rot = (uint8_t)rateOfChange;
 				_FanConfigurationRegisters[fanID] = (_FanConfigurationRegisters[fanID] & 0b11100011) | (rot << 2);
 				return _writeToRegister(FAN1_DYNAMICS_ADDRESS + fanID, _FanConfigurationRegisters[fanID]);
 			}
