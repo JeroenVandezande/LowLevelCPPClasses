a5baf9896e6537f87ed8f0b5ddbe5dfb85c6a8a2
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2025-06-22 17:19:13 +0200

    Introduce I2C multiplexer base classes
    Implements foundational I2C multiplexer functionality with base and channel classes, enabling channel switching and I2C operations across multiple channels. Provides structure for derived classes to customize specific multiplexer behavior.

---
+++ Devices/I2CMultiplexers/I2CMultiplexer_base.cpp
@@ -0,0 +1,62 @@
+//
+// Created by Jeroe on 2025-06-22.
+//
+
+#include "I2CMultiplexer_base.h"
+
+namespace LowLevelEmbedded::Devices::I2CMultiplexers
+{
+
+bool I2CMultiplexer_channel::I2C_WriteMethod(uint8_t address, uint8_t *data, size_t length)
+{
+  if (_channelnumber != _parent->_multiplexerchannel)
+  {
+    _parent->SwitchMultiplexerChannel(_channelnumber);
+    _parent->_multiplexerchannel = _channelnumber;
+  }
+  return _parent->_I2CAccess->I2C_WriteMethod(address, data, length);
+}
+
+bool I2CMultiplexer_channel::I2C_ReadMethod(uint8_t address, uint8_t *data, size_t length)
+{
+  if (_channelnumber != _parent->_multiplexerchannel)
+  {
+    _parent->SwitchMultiplexerChannel(_channelnumber);
+    _parent->_multiplexerchannel = _channelnumber;
+  }
+  return _parent->_I2CAccess->I2C_ReadMethod(address, data, length);
+}
+
+bool I2CMultiplexer_channel::I2C_ReadWriteMethod(uint8_t address, uint8_t *data, size_t readLength,
+                         size_t writeLength)
+{
+  if (_channelnumber != _parent->_multiplexerchannel)
+  {
+    _parent->SwitchMultiplexerChannel(_channelnumber);
+    _parent->_multiplexerchannel = _channelnumber;
+  }
+  return _parent->_I2CAccess->I2C_ReadWriteMethod(address, data, readLength, writeLength);
+}
+
+bool I2CMultiplexer_channel::I2C_Mem_Read(uint8_t address, uint8_t memAddress, uint8_t memAddsize,
+                  uint8_t *data, size_t readLength)
+{
+  if (_channelnumber != _parent->_multiplexerchannel)
+  {
+    _parent->SwitchMultiplexerChannel(_channelnumber);
+    _parent->_multiplexerchannel = _channelnumber;
+  }
+  return _parent->_I2CAccess->I2C_Mem_Read(address, memAddress, memAddsize, data, readLength);
+}
+
+bool I2CMultiplexer_channel::I2C_IsDeviceReady(uint8_t address)
+{
+  if (_channelnumber != _parent->_multiplexerchannel)
+  {
+    _parent->SwitchMultiplexerChannel(_channelnumber);
+    _parent->_multiplexerchannel = _channelnumber;
+  }
+  return _parent->_I2CAccess->I2C_IsDeviceReady(address);
+}
+
+}
