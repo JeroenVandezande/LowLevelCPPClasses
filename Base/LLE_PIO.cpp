54e8f9b7457a59f058316adcd3c0193a81451d82
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2025-08-18 14:14:52 +0200

    Update microcontroller support
    Adds new STM32IOPin class supporting various STM32 families.
    Updates microlog package to version 6.5.0 for enhanced logging.
    Improves method signatures in LLE_PIO for const-correctness.

--- Base/LLE_PIO.cpp
+++ Base/LLE_PIO.cpp
@@ -8,7 +8,7 @@
 namespace LowLevelEmbedded
 {
 
-	void IPIO_8::SetBit(uint8_t bitNumber)
+	void IPIO_8::SetBit(const uint8_t bitNumber)
 	{
 		uint8_t modifiedPort = this->_shadow | 1UL << bitNumber;
 		this->WritePort(modifiedPort);
@@ -16,7 +16,7 @@ namespace LowLevelEmbedded
 	}
 
 
-	void IPIO_8::ClearBit(uint8_t bitNumber)
+	void IPIO_8::ClearBit(const uint8_t bitNumber)
 	{
 		uint8_t modifiedPort = this->_shadow & ~(1UL << bitNumber);
 		this->WritePort(modifiedPort);
@@ -24,19 +24,19 @@ namespace LowLevelEmbedded
 	}
 
 
-	void IPIO_8::ToggleBit(uint8_t bitNumber)
+	void IPIO_8::ToggleBit(const uint8_t bitNumber)
 	{
 		uint8_t modifiedPort = this->_shadow ^ 1UL << bitNumber;
 		this->WritePort(modifiedPort);
 		this->_shadow = modifiedPort;
 	}
 
-	bool IPIO_8::GetBit(uint8_t bitNumber)
+	bool IPIO_8::GetBit(const uint8_t bitNumber)
 	{
 		return (this->ReadPort() & (1UL << bitNumber)) != 0;
 	}
 
-	IOPIN* IPIO_8::CreateIOPin(uint8_t bitNumber)
+	IOPIN* IPIO_8::CreateIOPin(const uint8_t bitNumber)
 	{
 		return new IOPIN_PIO8(this, bitNumber);
 	}
---
+++ Devices/PIOs/STM32IOPin.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "LLE_IOPIN.h"
+#include <cstdint>
+
+#if defined(__has_include)
+
+#if __has_include("stm32f0xx_ll_gpio.h")
+#include "stm32f0xx_ll_gpio.h"
+
+#elif __has_include("stm32f1xx_ll_gpio.h")
+#include "stm32f1xx_ll_gpio.h"
+
+#elif __has_include("stm32f2xx_ll_gpio.h")
+#include "stm32f2xx_ll_gpio.h"
+
+#elif __has_include("stm32f3xx_ll_gpio.h")
+#include "stm32f3xx_ll_gpio.h"
+
+#elif __has_include("stm32f4xx_ll_gpio.h")
+#include "stm32f4xx_ll_gpio.h"
+
+#elif __has_include("stm32f7xx_ll_gpio.h")
+#include "stm32f7xx_ll_gpio.h"
+
+#elif __has_include("stm32g0xx_ll_gpio.h")
+#include "stm32g0xx_ll_gpio.h"
+
+#elif __has_include("stm32g4xx_ll_gpio.h")
+#include "stm32g4xx_ll_gpio.h"
+
+#elif __has_include("stm32h7xx_ll_gpio.h")
+#include "stm32h7xx_ll_gpio.h"
+
+#elif __has_include("stm32l0xx_ll_gpio.h")
+#include "stm32l0xx_ll_gpio.h"
+
+#elif __has_include("stm32l1xx_ll_gpio.h")
+#include "stm32l1xx_ll_gpio.h"
+
+#elif __has_include("stm32l4xx_ll_gpio.h")
+#include "stm32l4xx_ll_gpio.h"
+
+#elif __has_include("stm32l5xx_ll_gpio.h")
+#include "stm32l5xx_ll_gpio.h"
+
+#elif __has_include("stm32u5xx_ll_gpio.h")
+#include "stm32u5xx_ll_gpio.h"
+
+#elif __has_include("stm32wbxx_ll_gpio.h")
+#include "stm32wbxx_ll_gpio.h"
+
+#elif __has_include("stm32wlxx_ll_gpio.h")
+#include "stm32wlxx_ll_gpio.h"
+
+#elif __has_include("stm32mp1xx_ll_gpio.h")
+#include "stm32mp1xx_ll_gpio.h"
+
+#else
+#error                                                                         \
+    "Could not detect STM32 family: no stm32*xx_ll_gpio.h header found in include path."
+#endif
+
+#endif // defined(__has_include)
+
+class STM32IOPin : public LowLevelEmbedded::IOPIN
+{
+private:
+  GPIO_TypeDef *pio_;
+  uint16_t pin_;
+
+public:
+  STM32IOPin(GPIO_TypeDef *GPIO_Port, uint16_t GPIO_Pin)
+  {
+    this->pio = GPIO_Port;
+    this->pin = GPIO_Pin;
+  }
+  void Set() final { LL_GPIO_SetOutputPin(pio_, pin_); };
+  void Clear() final { LL_GPIO_ResetOutputPin(pio_, pin_); };
+  bool GetValue() final
+  {
+    return (pio_->IDR & pin_) != (uint32_t)GPIO_PIN_RESET;
+  };
+  void Toggle() final { LL_GPIO_TogglePin(pio_, pin_); };
+};
