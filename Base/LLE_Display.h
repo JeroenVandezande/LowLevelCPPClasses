a2fa8e1fd0cc16b41d9fcf745823c4032b51c29e
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2025-09-19 16:02:52 +0200

    Refactors IDisplay interface
    Converts non-pure virtual functions to pure virtual
    functions in IDisplay interface for better abstraction
    and enforce implementation in derived classes.
    Removes unused SetContrast function for simplification.

--- Base/LLE_Display.h
+++ Base/LLE_Display.h
@@ -29,23 +29,22 @@ namespace LowLevelEmbedded
     {
     public:
         using color_type = DisplayColorType;
-        virtual void Init();
-        void Fill(color_type color);
-        void UpdateScreen();
-        void DrawPixel(uint8_t x, uint8_t y, color_type color);
-        char WriteChar(char ch, Display_Font_t Font, color_type color);
-        char WriteString(char* str, Display_Font_t Font, color_type color);
-        void SetCursor(uint8_t x, uint8_t y);
-        void Line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, color_type color);
-        void DrawArc(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle, uint16_t sweep,
-                     color_type color);
-        void DrawArcWithRadiusLine(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle,
-                                   uint16_t sweep, color_type color);
-        void DrawCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r, color_type color);
-        void FillCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r, color_type par_color);
-        void Polyline(const DISPLAY_VERTEX* par_vertex, uint16_t par_size, color_type color);
-        void DrawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, color_type color);
-        void FillRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, color_type color);
+        virtual void Init() = 0;
+        virtual void Fill(color_type color) = 0;
+        virtual void DrawPixel(uint8_t x, uint8_t y, color_type color) = 0;
+        virtual char WriteChar(char ch, Display_Font_t Font, color_type color) = 0;
+        virtual char WriteString(char* str, Display_Font_t Font, color_type color) = 0;
+        virtual void SetCursor(uint8_t x, uint8_t y) = 0;
+        virtual void Line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, color_type color) = 0;
+        virtual void DrawArc(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle, uint16_t sweep,
+                     color_type color) = 0;
+        virtual void DrawArcWithRadiusLine(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle,
+                                   uint16_t sweep, color_type color) = 0;
+        virtual void DrawCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r, color_type color) = 0;
+        virtual void FillCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r, color_type par_color) = 0;
+        virtual void Polyline(const DISPLAY_VERTEX* par_vertex, uint16_t par_size, color_type color) = 0;
+        virtual void DrawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, color_type color) = 0;
+        virtual void FillRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, color_type color) = 0;
 
         /**
                  * @brief Invert color of pixels in rectangle (include border)
@@ -56,21 +55,13 @@ namespace LowLevelEmbedded
                  * @param y2 Y Coordinate of bottom right corner
                  * @return SSD1306_Error_t status
                  */
-        Display_Error_t InvertRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+        virtual Display_Error_t InvertRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2) = 0;
 
-        void DrawBitmap(uint8_t x, uint8_t y, const unsigned char* bitmap, uint8_t w, uint8_t h, color_type color);
+        virtual void DrawBitmap(uint8_t x, uint8_t y, const unsigned char* bitmap, uint8_t w, uint8_t h, color_type color) = 0;
 
-        /**
-                 * @brief Sets the contrast of the display.
-                 * @param[in] value contrast to set.
-                 * @note Contrast increases as the value increases.
-                 * @note RESET = 7Fh.
-                 */
-        void SetContrast(const uint8_t value);
+        virtual void SetDisplayOn(const bool on) = 0;
 
-        void SetDisplayOn(const bool on);
-
-        bool GetDisplayOn() const;
+        virtual bool GetDisplayOn() const = 0;
 
         virtual void Reset()
         {
