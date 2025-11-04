9cf815735774ed387769c563a12c4a00212bcb7b
Jeroen Vandezande <Jeroen.Vandezande@gmail.com>
2025-11-04 17:50:03 +0100

    Added a method to get the miliseconds since startup

--- Utilities/Delay.h
+++ Utilities/Delay.h
@@ -15,6 +15,10 @@ namespace LowLevelEmbedded
 
         /// Usage: LowLevelEmbedded::Utility::Delay_us = DelayUs_DWT;
         inline std::function<void(uint32_t delay)> Delay_us;
+
+       /// returns the number of milliseconds since the system started
+        inline std::function<uint32_t()> millis;
+
     }
 }
 
