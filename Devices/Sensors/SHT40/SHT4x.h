7378a7125fac551b785712c6dc2077af0a1c98e9
Jeroen Vandezande <Jeroen.Vandezande@gmail.com>
2025-11-05 16:07:11 +0100

    Pragma once; flatten namespaces
    Replaces header include guards with pragma once to reduce boilerplate and prevent guard mismatches.
    Flattens nested namespaces using modern C++ syntax for clarity and maintainability.
    Keeps public API and behavior unchanged.

--- Devices/Sensors/SHT40/SHT4x.h
+++ Devices/Sensors/SHT40/SHT4x.h
@@ -2,52 +2,43 @@
 // Created by DanaNatov on 2025-10-20.
 //
 
-#ifndef HOODCONNECTIONMASTER_SHT40_H
-#define HOODCONNECTIONMASTER_SHT40_H
+#pragma once
+
 #include "LLE_I2C.h"
 #include "LLE_Temp.h"
 
 #include <cstdint>
 
-namespace LowLevelEmbedded
+namespace LowLevelEmbedded::Devices::Sensors
 {
-    namespace Devices
+    typedef enum {
+        LOW,
+        MEDIUM,
+        HIGH,
+    } SHT4x_Precision;
+
+    typedef enum {
+        H20mW_100ms,
+        H20mW_1000ms,
+        H110mW_100ms,
+        H110mW_1000ms,
+        H200mW_100ms,
+        H200mW_1000ms,
+    } SHT4x_HeaterPreset;
+
+    class SHT4x : ITempSensor
     {
-        namespace Sensors
-        {
-            typedef enum {
-                LOW,
-                MEDIUM,
-                HIGH,
-            } SHT4x_Precision;
-
-            typedef enum {
-                H20mW_100ms,
-                H20mW_1000ms,
-                H110mW_100ms,
-                H110mW_1000ms,
-                H200mW_100ms,
-                H200mW_1000ms,
-            } SHT4x_HeaterPreset;
-
-            class SHT4x : ITempSensor
-            {
-                private:
-                    bool CheckCRC(uint16_t data, uint8_t crc);
-                    II2CAccess* _i2cAccess;
-                    uint8_t _address;
-                public:
-                    static constexpr uint8_t DEFAULT_I2C_ADDRESS = 0x88;
-                    SHT4x(II2CAccess* i2c, uint8_t address = DEFAULT_I2C_ADDRESS);
-                    void Reset();
-                    bool ReadTemperatureAndHumidity(float& temperatureC, float& humidity, SHT4x_Precision precision = HIGH);
-                    bool ActivateHeater(float& temperatureC, float& humidity, SHT4x_HeaterPreset preset);
-                    void GetTemperature(float& temperature) override;
-                    uint32_t GetSerialNumber();
-            };
-        }
-    }
+    private:
+        bool CheckCRC(uint16_t data, uint8_t crc);
+        II2CAccess* _i2cAccess;
+        uint8_t _address;
+    public:
+        static constexpr uint8_t DEFAULT_I2C_ADDRESS = 0x88;
+        SHT4x(II2CAccess* i2c, uint8_t address = DEFAULT_I2C_ADDRESS);
+        void Reset();
+        bool ReadTemperatureAndHumidity(float& temperatureC, float& humidity, SHT4x_Precision precision = HIGH);
+        bool ActivateHeater(float& temperatureC, float& humidity, SHT4x_HeaterPreset preset);
+        void GetTemperature(float& temperature) override;
+        uint32_t GetSerialNumber();
+    };
 }
-
-
-#endif //HOODCONNECTIONMASTER_SHT40_H
