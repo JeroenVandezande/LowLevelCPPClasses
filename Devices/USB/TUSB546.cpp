46e6399e605a063ea90c6d29a172a19524c80dd1
Jeroen Vandezande <Jeroen.Vandezande@gmail.com>
2025-11-05 15:54:51 +0100

    Adds TUSB546 I2C driver
    Introduces an I2C driver for a TI USB/DP redriver/mux, enabling control of mode configuration and cable orientation.
    
    Adds read-modify-write helpers for safe register updates and a readiness probe for device detection.

---
+++ Devices/USB/TUSB546.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Jeroen Vandezande on 05/11/2025.
+//
+
+#include "TUSB546.h"
+
+namespace LowLevelEmbedded::Devices::USBDevices
+{
+    bool TUSB546::writeRegister(const uint8_t address, const uint8_t value) const
+    {
+        uint8_t data[2] = { address, value };
+        return i2c_->I2C_WriteMethod(i2cAddress_, data, 2);
+    }
+
+    uint8_t TUSB546::readRegister(uint8_t address) const
+    {
+        uint8_t returndata;
+        i2c_->I2C_WriteMethod(i2cAddress_, &address, 1);
+        i2c_->I2C_ReadMethod(i2cAddress_, &returndata, 1);
+        return returndata;
+    }
+
+    bool TUSB546::rmwRegister(uint8_t address, uint8_t mask, uint8_t value) const
+    {
+        uint8_t reg = readRegister(address);
+        reg &= ~mask;
+        reg |= value;
+        return writeRegister(address, reg);
+    }
+
+    bool TUSB546::SetConfiguration(const Configuration configuration) const
+    {
+        return rmwRegister(0x0A, 0b00000011, static_cast<uint8_t>(configuration));
+    }
+
+    bool TUSB546::SetFlipped(const bool flipped) const
+    {
+        return rmwRegister(0x0A, 0b00000100, flipped ? 0b00000100 : 0);
+    }
+
+    bool TUSB546::IsDeviceReady()
+    {
+        return i2c_->I2C_IsDeviceReady(i2cAddress_);
+    }
+
+
+}
---
+++ Devices/USB/TUSB546.h
@@ -0,0 +1,50 @@
+//
+// Created by Jeroen Vandezande on 05/11/2025.
+//
+
+#pragma once
+#include "LLE_I2C.h"
+#include <cstdint>
+
+namespace LowLevelEmbedded::Devices::USBDevices
+{
+    class TUSB546 : public LowLevelEmbedded::II2CDevice
+    {
+    public:
+        enum Configuration
+        {
+            Disabled = 0,
+            USB_Only = 1,
+            DisplayPort_Only = 2,
+            DisplayPort_USB = 3,
+        };
+
+        explicit TUSB546(LowLevelEmbedded::II2CAccess* i2c, const uint8_t i2cAddress)
+        {
+            i2c_        = i2c;
+            i2cAddress_ = i2cAddress;
+        }
+
+        bool SetConfiguration(Configuration configuration) const;
+
+        bool SetFlipped(bool flipped) const;
+
+        bool IsDeviceReady() override;
+
+    private:
+        uint8_t i2cAddress_;
+        LowLevelEmbedded::II2CAccess* i2c_;
+        bool writeRegister(uint8_t address, uint8_t value) const;
+        [[nodiscard]] uint8_t readRegister(uint8_t address) const;
+        /**
+         * Modifies specific bits of a register at the given address by first reading the
+         * register value, applying a bitwise mask, and then writing the updated value back.
+         *
+         * @param address The I2C address of the register to read and modify.
+         * @param mask The bitwise mask that specifies which bits to modify. Bits set to 1 in
+         *             the mask are those that will be cleared before applying the new value.
+         * @param value The new value to apply to the masked bits in the register.
+         */
+        bool rmwRegister(uint8_t address, uint8_t mask, uint8_t value) const;
+    };
+}
